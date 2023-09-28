81596640a53439a0a4f84cdf4fcc5f662d6ae9be
ClicketyClackety <callumsykes@protonmail.com>
2023-09-28 18:35:34 -0400

    Removed dead classes

--- src/Program.cs
+++ src/Program.cs
@@ -1,39 +1,9 @@
-﻿using System.Net.Mime;
-using System.Text;
-
-using Crash.Server.Hubs;
+﻿using Crash.Server.Hubs;
 using Crash.Server.Model;
 using Crash.Server.Settings;
 
 namespace Crash.Server
 {
-	internal class HtmlResult : IResult
-	{
-		private readonly string _html;
-
-		public HtmlResult(string html)
-		{
-			_html = html;
-		}
-
-		public Task ExecuteAsync(HttpContext httpContext)
-		{
-			httpContext.Response.ContentType = MediaTypeNames.Text.Html;
-			httpContext.Response.ContentLength = Encoding.UTF8.GetByteCount(_html);
-			return httpContext.Response.WriteAsync(_html);
-		}
-	}
-
-	internal static class ResultsExtensions
-	{
-		public static IResult Html(this IResultExtensions resultExtensions, string html)
-		{
-			ArgumentNullException.ThrowIfNull(resultExtensions);
-
-			return new HtmlResult(html);
-		}
-	}
-
 	// TODO : Improve logging
 	public class Program
 	{
