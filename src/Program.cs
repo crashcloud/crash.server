e4cbb046930fcc80e258cdc679414a317f97ba27
Callum Sykes <callumsykes@protonmail.com>
2023-12-17 12:23:28 +0000

    Resolved HubOptions not being set

--- src/Program.cs
+++ src/Program.cs
@@ -5,76 +5,109 @@ using Crash.Server.Hubs;
 using Crash.Server.Model;
 
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Options;
 
 namespace Crash.Server
 {
 	public class Program
 	{
-		/// <summary>Creates an instance of the Crash WebApplication</summary>
-		public static WebApplication CreateApplication(params string[] args)
+
+		private class CrashServerCreator
 		{
-			var argHandler = new ArgumentHandler();
 
-			argHandler.EnsureDefaults();
-			argHandler.ParseArgs(args);
-			if (argHandler.Exit)
+			private readonly ArgumentHandler _argHandler;
+			private readonly string[] _args;
+			private WebApplication _app { get; set; }
+
+			internal CrashServerCreator(params string[] args)
 			{
-				return null;
+				_args = args;
+				
+				_argHandler = new();
+				_argHandler.EnsureDefaults();
+				_argHandler.ParseArgs(args);
 			}
-
-			if (argHandler.ResetDB && File.Exists(argHandler.DatabaseFileName))
-			{
-				File.Delete(argHandler.DatabaseFileName);
-			}
-
-			var webBuilder = WebApplication.CreateBuilder(args);
-			var crashLogger = new CrashLoggerProvider();
-			webBuilder.Logging.AddProvider(crashLogger);
-
-			webBuilder.Services.AddSignalR()
-				.AddHubOptions<CrashHub>(hubOptions =>
-				{
-					var crashConfig = webBuilder.Configuration.GetRequiredSection("Crash");
-					var signalRConfig = crashConfig.GetRequiredSection("SignalR");
-					hubOptions = signalRConfig.GetRequiredSection("CrashHub").Get<HubOptions<CrashHub>>();
-				})
-				.AddJsonProtocol(jsonOptions =>
-				{
-					var crashConfig = webBuilder.Configuration.GetRequiredSection("Crash");
-					var signalRConfig = crashConfig.GetRequiredSection("SignalR");
-					jsonOptions.PayloadSerializerOptions =
-						signalRConfig.GetRequiredSection("Json").Get<JsonSerializerOptions>();
-				});
-
-			webBuilder.Services.AddDbContext<CrashContext>(options =>
-				options.UseSqlite($"Data Source={argHandler.DatabaseFileName}"));
 			
-			// Do we need this?
-			webBuilder.WebHost.UseUrls(argHandler.URL);
-			webBuilder.Services.AddRazorPages();
-
-			var app = webBuilder.Build();
-
-			app.MapHub<CrashHub>("/Crash");
-
-			if (app.Environment.IsDevelopment())
+			/// <summary>Creates an instance of the Crash WebApplication</summary>
+			internal WebApplication CreateApplication()
 			{
-				app.MapGet("/logging", () => string.Join("\n", crashLogger.Logger.Messages));
-				app.MapGet("/config", () => app.Configuration.AsEnumerable());
+				if (_argHandler.Exit)
+				{
+					return null;
+				}
+
+				if (_argHandler.ResetDB && File.Exists(_argHandler.DatabaseFileName))
+				{
+					File.Delete(_argHandler.DatabaseFileName);
+				}
+
+				var webBuilder_ = WebApplication.CreateBuilder(_args);
+				var crashLogger = new CrashLoggerProvider();
+				webBuilder_.Logging.AddProvider(crashLogger);
+
+				// Do we need this?
+				webBuilder_.WebHost.UseUrls(_argHandler.URL);
+				webBuilder_.Services.AddRazorPages();
+
+				webBuilder_.Services.AddDbContext<CrashContext>(options =>
+					options.UseSqlite($"Data Source={_argHandler.DatabaseFileName}"));
+
+				webBuilder_.Services.AddSignalR()
+					.AddHubOptions<CrashHub>(ConfigureCrashHuhhubOptions)
+					.AddJsonProtocol(ConfigureJsonOptions);
+
+				_app = webBuilder_.Build();
+
+				if (_app.Environment.IsDevelopment())
+				{
+					_app.MapGet("/logging", () => string.Join("\n", crashLogger.Logger.Messages));
+					_app.MapGet("/config", () => _app.Configuration.AsEnumerable());
+					_app.MapGet("/services", () =>
+					{
+						var crashHubOptionsService = _app.Services.GetService<IConfigureOptions<HubOptions<CrashHub>>>();
+						var connectionHandler = _app.Services.GetService<HubConnectionHandler<CrashHub>>();
+						;
+					});
+				}
+
+				_app.UseHttpsRedirection();
+				_app.UseStaticFiles();
+				_app.UseRouting();
+				_app.MapRazorPages();
+				_app.MigrateDatabase<CrashContext>();
+				_app.MapHub<CrashHub>("/Crash");
+
+				return _app;
 			}
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
-			app.UseRouting();
-			app.MapRazorPages();
+			private void ConfigureJsonOptions(JsonHubProtocolOptions jsonOptions)
+			{
+				var crashConfig = _app.Configuration.GetRequiredSection("Crash");
+				var signalRConfig = crashConfig.GetRequiredSection("SignalR");
+				jsonOptions.PayloadSerializerOptions =
+					signalRConfig.GetRequiredSection("Json").Get<JsonSerializerOptions>();
+			}
 
-			app.MigrateDatabase<CrashContext>();
-			return app;
+			private void ConfigureCrashHuhhubOptions(HubOptions<CrashHub> hubOptions)
+			{
+				var crashConfig = _app.Configuration.GetRequiredSection("Crash");
+				var signalRConfig = crashConfig.GetRequiredSection("SignalR");
+				var crashHubOptions = signalRConfig.GetRequiredSection("CrashHub").Get<HubOptions<CrashHub>>();
+				
+				// TODO : Is there a way to deserialize the Section TO the hubOptions object? Below feels redundant
+				hubOptions.MaximumParallelInvocationsPerClient = crashHubOptions.MaximumParallelInvocationsPerClient;
+				hubOptions.MaximumReceiveMessageSize = crashHubOptions.MaximumReceiveMessageSize;
+				hubOptions.StreamBufferCapacity = crashHubOptions.StreamBufferCapacity;
+				hubOptions.KeepAliveInterval = crashHubOptions.KeepAliveInterval;
+				hubOptions.HandshakeTimeout = crashHubOptions.HandshakeTimeout;
+				hubOptions.EnableDetailedErrors = crashHubOptions.EnableDetailedErrors;
+			}
 		}
 
 		public static void Main(string[] args)
 		{
-			var app = CreateApplication(args);
+			var serverCreator = new CrashServerCreator(args);
+			var app = serverCreator.CreateApplication();
 			app.Run();
 		}
 	}
